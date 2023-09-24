2d50f202e2636e25f919ae7dc455bca8d8235227
Gabryel Bele <belegabryel19@gmail.com>
2023-09-24 12:10:24 -0300

    Crate Task Repository

---
+++ src/main/java/com/gabryelbele/todosimple/repositories/TaskRepository.java
@@ -0,0 +1,23 @@
+package com.gabryelbele.todosimple.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.gabryelbele.todosimple.models.Task;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByUser_id(Long id);
+
+    // @Query(value = "SELECT t FROM Task t WHERE t.user.id = :id")
+    // List<Task> findByUserId(@Param("id") Long id); // Correção: Adicione os
+    // parênteses em @Param("id")
+
+    // @Query(value = "SELECT * FROM task t WHERE t.user_id = :id", nativeQuery =
+    // true)
+    // List<Task> findByUserId(@Param("id") Long id);
+}
