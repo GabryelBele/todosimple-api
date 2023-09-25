d438d1b905a8406800fdf12c9539cb68e31279fd
Gabryel Bele <belegabryel19@gmail.com>
2023-09-24 23:35:22 -0300

    Create Task Service

--- src/main/java/com/gabryelbele/todosimple/services/UserService.java
+++ src/main/java/com/gabryelbele/todosimple/services/UserService.java
@@ -19,9 +19,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
     public User findById(Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException("Usuário não encontrado com o ID: " + id));
@@ -31,7 +28,6 @@ public class UserService {
     public User create(User user) {
         user.setId(null);
         user = this.userRepository.save(user);
-        this.taskRepository.saveAll(user.getTasks());
         return user;
     }
 
